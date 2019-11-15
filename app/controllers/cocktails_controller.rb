f85ce3c0f447bf2b37bc917263b1e2dba076cc50
Charlie Whight <cdwhight@gmail.com>
2019-11-15 11:25:06 +0000

    change root

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -50,6 +50,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :description, :review, :review_score)
+    params.require(:cocktail).permit(:name, :photo, :description, :review, :review_score)
   end
 end
