23cfbaee7161a57f9d539121a6be4b8c594e1507
Charlie Whight <cdwhight@gmail.com>
2019-11-15 15:35:41 +0000

    bug fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
   validates :content, presence: true
-  validates :score, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :score, numericality: { only_integer: true }, inclusion: { in: [1, 2, 3, 4, 5] }
 end
